7204beb4a884ca6a9ce06051f53efb4cf8f71069
Sam Bowen <samhbowen@gmail.com>
2025-01-11 11:55:52 -0800

    add user folder, home.ejs, edit.ejs

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
-const isSignedIn = require('./middleware/is-signed-in.js');
-const passUserToView = require('./middleware/pass-user-to-view.js');
-
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js');
 
-
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -27,11 +25,10 @@ mongoose.connection.on('connected', () => {
 
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
-
-app.use(express.static(path.join(__dirname, 'public'))); //middleware so that our app can read and apply our css rules
-
+app.use(express.static('public'));
+app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -39,6 +36,8 @@ app.use(
     saveUninitialized: true,
   })
 );
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   if (req.session.user) {
